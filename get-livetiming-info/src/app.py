3991d47d557f7859357bf753088dcad6e2717fa4
Matt Smallhouse <smallhou@bc.edu>
2023-09-25 22:38:19 -0400

    merged functionality to scrape livetiming. Gets all names and times

--- get-livetiming-info/src/app.py
+++ get-livetiming-info/src/app.py
@@ -7,7 +7,20 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
+URL = "https://www.live-timing.com/race2.php?r=248437&u=0"
 
+def clean_name(name):
+    # prevents from breaking if name has no comma for some reason
+    if "," not in name:
+        print(f"skipping name: {name}")
+        return ""
+
+    name = name.lower()
+    name = name.split(",")
+    if len(name) >= 2:
+        # remove leading whitespace from first names, left over from splitting
+        name[1] = name[1][1:-1]
+    return name
 
 def handler(event=None, context=None):
     chrome_options = webdriver.ChromeOptions()
@@ -25,17 +38,49 @@ def handler(event=None, context=None):
     #chrome_options.add_argument("--data-path=/tmp/chrome-user-data")
     #chrome_options.add_argument("--disk-cache-dir=/tmp/chrome-user-data")
 
-    service = Service(executable_path=r'/opt/chromedriver')
+    chrome_service = Service(executable_path=r'/opt/chromedriver')
 
-    chrome = webdriver.Chrome(service=service, options=chrome_options)
-    chrome.get("https://cloudbytes.dev/")
-    description = chrome.find_element(By.NAME, "description").get_attribute("content")
-    print(description)
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": description,
-            }
-        ),
-    }
+    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    driver.get(URL)
+    driver.implicitly_wait(10)
+
+
+    full_names = []
+    times = []
+    table = driver.find_element(By.ID, "resultTable")
+    rows = table.find_elements(By.CLASS_NAME, "table")
+    for row in rows:
+        cols = row.find_elements(By.XPATH, ".//td")
+        # only get table elements rendered with all fields
+        if len(cols) >= 2:
+            full_names.append(cols[2].text)
+            # [:-1] splice removes trailing whitespace
+            time = cols[-1].text[:-1]
+            # calculate time in seconds, but only for those who finished
+
+            if not time:
+                time = float(-1)
+            # edge case for times under a minute
+            elif ":" not in time:
+                time = float(time)
+            else:
+                minutes = time.split(":")[0]
+                seconds = time.split(":")[1]
+                time = float(minutes)*60 + float(seconds)
+            times.append(time)
+
+    # 2d list of names, with each inner list of format [last_name, first_name]
+    split_names = []
+    for name in full_names:
+        name = clean_name(name)
+        # function returns "" on error
+        if not name:
+            continue
+        split_names.append(name)
+
+    print(len(split_names))
+    print(len(times))
+
+    # Close webdriver and chrome service
+    driver.quit()
+    chrome_service.stop()
