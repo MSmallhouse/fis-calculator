214acff2f822f3cdbe02d630b9add8107bf6f0ad
Matt Smallhouse <smallhou@bc.edu>
2023-12-12 16:31:22 -0500

    form includes event and type of competition

--- website/app.js
+++ website/app.js
@@ -1,6 +1,8 @@
 const lambdaURL = "https://imvf3e6jot4nih4i5th43endj40jbfqa.lambda-url.us-east-2.on.aws/";
 
 const url = document.getElementById("urlInput");
+const eventSelector = document.getElementById("eventSelector");
+const minPenalty = document.getElementById("minPenalty");
 const submitResponse = document.getElementById("response");
 const form = document.getElementById("userInfo");
 form.setAttribute("action", lambdaURL);
@@ -8,8 +10,12 @@ form.setAttribute("method", "get");
 
 form.onsubmit = e => {
     e.preventDefault();
+    // attach form responses to url as query string
+    const requestURL = `${lambdaURL}?url=${encodeURIComponent(url.value)}
+                        &min-penalty=${minPenalty.value}
+                        &event=${eventSelector.value}`
 
-    let points = fetch(`${lambdaURL}?url=${encodeURIComponent(url.value)}`)
+    let points = fetch(requestURL)
 
     points.then(response => response.json())
         .then(data => {
