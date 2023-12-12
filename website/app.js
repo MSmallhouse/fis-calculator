8df9a44f73214890754dfb90f3ade9dd8d5e41b5
Matt Smallhouse <smallhou@bc.edu>
2023-12-11 22:29:47 -0500

    form info correctly sends to aws lambda

--- website/app.js
+++ website/app.js
@@ -9,9 +9,9 @@ form.setAttribute("method", "get");
 form.onsubmit = e => {
     e.preventDefault();
 
+    let points = fetch(`${lambdaURL}?url=${encodeURIComponent(url.value)}`)
 
-    fetch(`${lambdaURL}?url=${encodeURIComponent(url)}`)
-        .then(response => response.json())
+    points.then(response => response.json())
         .then(data => {
             // handle response from Lambda function
             console.log(data);
@@ -20,51 +20,3 @@ form.onsubmit = e => {
             console.log("Error: ", error);
         });
 }
-
-    /*let data = {};
-    Array.from(form).map(input => (data[input.id] = input.value));
-    console.log("Sending: ", JSON.stringify(data));
-    submitResponse.innerHTML = "Sending...";*/
-
-    // Create the AJAX request
-    /*let xhr = new XMLHttpRequest();
-    xhr.open(form.method, lambdaEndpoint, true);
-    xhr.setRequestHeader("Accept", "application/json; charset=utf-8");
-    xhr.setRequestHeader("Content-Type", "application/json; charset-utf-8");
-
-    // send the collected data as JSON
-    xhr.send(JSON.stringify(data));
-
-    xhr.onloadend = response => {
-        if (response.target.status === 200) {
-            form.reset();
-            submitResponse.innerHTML = "Form Submitted";
-        } else {
-            submitResponse.innerHTML = "Submission Error!";
-            console.error(JSON.parse(response));
-        }
-    }
-    
-}*/
-
-
-/*const urlForm = document.getElementById("urlInput");
-
-urlForm.addEventListener("submit", function(event) {
-    // prevent default form submission
-    event.preventDefault();
-
-    const url = document.getElementById("urlInput");
-    console.log("pressed");
-
-    // send GET request
-    fetch(`${lambdaEndpoint}?url=${encodeURIComponent(url)}`)
-        .then(response => response.json())
-        .then(data => {
-            // handle response from Lambda function
-            console.log(data);
-        })
-        .catch(error => {
-            console.log("Error: ", error);
-        });
-});*/
