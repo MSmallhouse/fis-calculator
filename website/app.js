4af3aabf2bd94b460c8101e81b3e40b8ffac76c6
Matt Smallhouse <smallhou@bc.edu>
2024-01-04 17:21:47 -0500

    clear screen when form submitted a second time

--- website/app.js
+++ website/app.js
@@ -3,13 +3,14 @@ const lambdaURL = "https://imvf3e6jot4nih4i5th43endj40jbfqa.lambda-url.us-east-2
 const url = document.getElementById("urlInput");
 const eventSelector = document.getElementById("eventSelector");
 const minPenalty = document.getElementById("minPenalty");
-const submitResponse = document.getElementById("response");
 const form = document.getElementById("userInfo");
+const results = document.getElementById("results")
 form.setAttribute("action", lambdaURL);
 form.setAttribute("method", "get");
 
 form.onsubmit = e => {
     e.preventDefault();
+    results.innerHTML = "";
     // attach form responses to url as query string
     const requestURL = `${lambdaURL}?url=${encodeURIComponent(url.value)}
                         &min-penalty=${minPenalty.value}
@@ -22,11 +23,13 @@ form.onsubmit = e => {
             for (let i=0; i<data.length; i++) {
                 const header = document.createElement("h4");
                 header.textContent = `${i+1}. ${data[i]}`;
-                document.body.append(header);
-
+                results.append(header);
             }
         })
         .catch(error => {
             console.log("Error: ", error);
+            const header = document.createElement("h4");
+            header.textContent = "Something went wrong...";
+            document.body.append(header);
         });
 }
