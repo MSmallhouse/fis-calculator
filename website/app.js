5ff8e0249fe2c6b451c0062aa897c09c21a79e82
MSmallhouse <smallhou@bc.edu>
2024-12-22 00:20:35 -0500

    add sticky contact bar on scroll

--- website/app.js
+++ website/app.js
@@ -1,93 +1,121 @@
-const lambdaURL = "https://imvf3e6jot4nih4i5th43endj40jbfqa.lambda-url.us-east-2.on.aws/";
-
-const url = document.getElementById("urlInput");
-const eventSelector = document.getElementById("eventSelector");
-const minPenalty = document.getElementById("minPenalty");
-const form = document.getElementById("userInfo");
-const loader = document.getElementById("loader");
-const results = document.getElementById("results");
-const submitBtn = document.getElementById("submitBtn");
-form.setAttribute("action", lambdaURL);
-form.setAttribute("method", "get");
-
-const select = document.querySelectorAll("select");
-select.forEach(selectElement => {
-    selectElement.onchange = function() {
-        selectElement.classList.add('option-selected-color');
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    formSubmitBehavior();
+    applyStyling();
+    toggleHeaderOnScroll();
 });
 
-submitBtn.addEventListener("click", () => {
-    loader.style.display = "block";
-})
+function formSubmitBehavior() {
+    const lambdaURL = "https://imvf3e6jot4nih4i5th43endj40jbfqa.lambda-url.us-east-2.on.aws/";
+    const url = document.getElementById("urlInput");
+    const eventSelector = document.getElementById("eventSelector");
+    const minPenalty = document.getElementById("minPenalty");
+    const form = document.getElementById("userInfo");
+    const loader = document.getElementById("loader");
+    const results = document.getElementById("results");
 
-form.onsubmit = e => {
-    e.preventDefault();
-    results.innerHTML = "";
-    // attach form responses to url as query string
-    const requestURL = `${lambdaURL}?url=${encodeURIComponent(url.value)}
-                        &min-penalty=${minPenalty.value}
-                        &event=${eventSelector.value}`
+    form.setAttribute("action", lambdaURL);
+    form.setAttribute("method", "get");
 
-    fetch(requestURL).then(response => response.json())
-        .then(data => {
-            loader.style.display = "none";
-            // handle response from Lambda function
-            console.log(data);
-            if (data.notFound) {
-                const warning = document.createElement("p");
-                warning.textContent = "results might be off, points not found for:";
-                const notFound = document.createElement("p");
-                notFound.textContent = `${data.notFound}`
-                results.append(warning);
-                results.append(notFound);
-            }
+    form.onsubmit = e => {
+        e.preventDefault();
+        results.innerHTML = "";
+        // attach form responses to url as query string
+        const requestURL = `${lambdaURL}?url=${encodeURIComponent(url.value)}
+                            &min-penalty=${minPenalty.value}
+                            &event=${eventSelector.value}`
 
-            const table = document.createElement("table");
-            const tableHead = document.createElement("thead");
-            const tableBody = document.createElement("tbody");
-            const headerRow = document.createElement("tr");
-            headerRow.innerHTML = `
-                <th>Pl</th>
-                <th>Name</th>
-                <th>Points</th>
-                <th>Score</th>`;
-            tableHead.append(headerRow);
-            table.append(tableHead);
-
-            data.results.forEach(result => {
-                const row = document.createElement("tr");
-                const placeCell = document.createElement("td");
-                const nameCell = document.createElement("td");
-                const pointsCell = document.createElement("td");
-                const scoreCell = document.createElement("td");
-
-                placeCell.textContent = result.place;
-                nameCell.textContent = result.name;
-                nameCell.classList.add("text-break");
-                pointsCell.textContent = result.points.toFixed(2);
-                pointsCell.classList.add("text-center");
-                scoreCell.textContent = result.score.toFixed(2);
-                scoreCell.classList.add("text-center");
-                if (result.score < result.points) {
-                    scoreCell.classList.add("personal-best");
+        fetch(requestURL).then(response => response.json())
+            .then(data => {
+                loader.style.display = "none";
+                // handle response from Lambda function
+                if (data.notFound) {
+                    const warning = document.createElement("p");
+                    warning.textContent = "results might be off, points not found for:";
+                    const notFound = document.createElement("p");
+                    notFound.textContent = `${data.notFound}`
+                    results.append(warning);
+                    results.append(notFound);
                 }
-                row.append(placeCell);
-                row.append(nameCell);
-                row.append(pointsCell);
-                row.append(scoreCell);
-                tableBody.append(row);
-            });
 
-            table.append(tableBody);
-            results.append(table)
-            results.scrollIntoView({behavior: 'smooth'});
-        })
-        .catch(error => {
-            loader.style.display = "none";
-            console.log("Error: ", error);
-            const header = document.createElement("h4");
-            header.textContent = "Something went wrong...";
-            results.append(header);
-        });
+                const table = document.createElement("table");
+                const tableHead = document.createElement("thead");
+                const tableBody = document.createElement("tbody");
+                const headerRow = document.createElement("tr");
+                headerRow.innerHTML = `
+                    <th>Pl</th>
+                    <th>Name</th>
+                    <th>Points</th>
+                    <th>Score</th>`;
+                tableHead.append(headerRow);
+                table.append(tableHead);
+
+                data.results.forEach(result => {
+                    const row = document.createElement("tr");
+                    const placeCell = document.createElement("td");
+                    const nameCell = document.createElement("td");
+                    const pointsCell = document.createElement("td");
+                    const scoreCell = document.createElement("td");
+
+                    placeCell.textContent = result.place;
+                    nameCell.textContent = result.name;
+                    nameCell.classList.add("text-break");
+                    pointsCell.textContent = result.points.toFixed(2);
+                    pointsCell.classList.add("text-center");
+                    scoreCell.textContent = result.score.toFixed(2);
+                    scoreCell.classList.add("text-center");
+                    if (result.score < result.points) {
+                        scoreCell.classList.add("personal-best");
+                    }
+                    row.append(placeCell);
+                    row.append(nameCell);
+                    row.append(pointsCell);
+                    row.append(scoreCell);
+                    tableBody.append(row);
+                });
+
+                table.append(tableBody);
+                results.append(table)
+                results.scrollIntoView({behavior: 'smooth'});
+            })
+            .catch(error => {
+                loader.style.display = "none";
+                console.log("Error: ", error);
+                const header = document.createElement("h4");
+                header.textContent = "Something went wrong...";
+                results.append(header);
+            });
+    }
+
+}
+
+function applyStyling() {
+    const select = document.querySelectorAll("select");
+    const submitBtn = document.getElementById("submitBtn");
+
+    select.forEach(selectElement => {
+        selectElement.onchange = function() {
+            selectElement.classList.add('option-selected-color');
+        }
+    });
+
+    submitBtn.addEventListener("click", () => {
+        loader.style.display = "block";
+    })
+}
+
+function toggleHeaderOnScroll() {
+  const header = document.querySelector('.contact');
+  let lastScroll = 0;
+
+  window.addEventListener('wheel', function () {
+    let scroll = window.scrollY || this.document.documentElement.scrollTop;
+    if(scroll == lastScroll) {
+      return;
+    } else if(scroll > lastScroll) { // hide header on scroll down
+      header.style.top = '-120px';
+    } else { // show header on scroll up
+      header.style.top = '0';
+    }
+    lastScroll = scroll;
+  });
 }
